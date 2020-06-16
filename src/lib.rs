d0e7823cfdf55f23d7a079892742f090aa312525
GrayJack <gr41.j4ck@gmail.com>
2020-06-16 06:53:01 -0300

    Impl debug for Janet

--- src/lib.rs
+++ src/lib.rs
@@ -2,41 +2,44 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
-use core::cmp::Ordering;
+use core::{cmp::Ordering, fmt};
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
+impl fmt::Debug for Janet {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.pad(core::any::type_name::<Self>())
+    }
+}
 
 impl PartialEq<Janet> for Janet {
-    fn eq(&self, other: &Janet) -> bool {
-        unsafe { janet_equals(*self, *other) != 0 }
-    }
+    fn eq(&self, other: &Janet) -> bool { unsafe { janet_equals(*self, *other) != 0 } }
 }
 
 impl Eq for Janet {}
 
 impl PartialOrd<Janet> for Janet {
     fn partial_cmp(&self, other: &Janet) -> Option<Ordering> {
-        let res = unsafe { janet_compare(*self,*other) };
+        let res = unsafe { janet_compare(*self, *other) };
 
         Some(match res {
             -1 => Ordering::Less,
             0 => Ordering::Equal,
             1 => Ordering::Greater,
-            _ => return None
+            _ => return None,
         })
     }
 }
 
 impl Ord for Janet {
     fn cmp(&self, other: &Self) -> Ordering {
-        let res = unsafe { janet_compare(*self,*other) };
+        let res = unsafe { janet_compare(*self, *other) };
 
         match res {
             -1 => Ordering::Less,
             0 => Ordering::Equal,
             1 => Ordering::Greater,
-            _ => unreachable!()
+            _ => unsafe { core::hint::unreachable_unchecked() },
         }
     }
 }
